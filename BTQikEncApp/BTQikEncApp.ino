69dc20e7d966755de0af38631d6294953a31094d
Megan <mshapiro@mines.edu>
2020-04-27 14:04:46 -0600

    Serial motors w/ J's app created

--- BTQikEncApp/BTQikEncApp.ino
+++ BTQikEncApp/BTQikEncApp.ino
@@ -20,9 +20,14 @@
 // SoftwareSerial is used to communicate with the Serial1
 #include <SoftwareSerial.h>
 #include <PololuQik.h>
-
+// Contstants
 #define TURN_TIME 150
-#define GAIN .85
+#define GAIN .95
+#define kp 5
+#define kd 1
+#define ki 1
+
+// Conversions
 #define COUNTS_PER_REV 979.62
 #define IN_PER_REV 10.039
 #define IN_PER_FT 12
@@ -30,21 +35,30 @@
 #define IN_PER_MILE IN_PER_FT*FT_PER_MILE
 #define SEC_PER_HR 3600
 #define CPS_TO_MPH IN_PER_REV*SEC_PER_HR/(COUNTS_PER_REV*IN_PER_MILE)
+
+//Pins
 #define leftEncoderB 2
 #define rightEncoderB 3
 #define leftEncoderA 4
 #define rightEncoderA 5
-//#define GAIN .87
+
 PololuQik2s9v1 qik(10, 11, 12);
 int systemState;
-int setA = 0;
-int setB = 0;
+volatile int setA = 0;
+volatile  int setB = 0;
 byte cmd2;
 
 volatile unsigned long leftCount = 0;
 volatile unsigned long rightCount = 0;
 float velocityL = 0;
 float velocityR = 0;
+float curVelL = 0;
+float curVelR = 0;
+float desVelL = 0;
+float desVelR = 0;
+
+
+
 void setup()
 {
   // Initialize Serial1 Software Serial port. Make sure the baud
@@ -75,62 +89,93 @@ void loop()
   // In loop() we continously check to see if a command has been
   //  received.
   if (Serial1.available()) {
-    readBT();
+    desVel = readBT();
   }
   if (systemState) {
-    qik.setSpeeds(setA, setB);
-    getVel();
-    //Serial1.print(setA);
-    //Serial1.print(" ");
-    //Serial1.println(setB);
+    getVelL();
+    getVelR();
+    setVel();
   } else {
-    qik.setSpeeds(0, 0);
+    setSpeeds(0, 0);
   }
   delay(100);
 }
 
+///////////////////////////////////
+void setVel(){
+  int uL = computeU(curVelL, desVelL);
+  int uR = computeU(curVelR, desVelR);
+}
+///////////////////////////////////
+void computeU(int curVel, int desVel){
+  volatile float errL = 0;
+  volatile float errSum = 0;
+  volatile unsigned long ti = 0;
+  volatile unsigned long ti_1 = 0;
+  float err = desVel - curVel;
+  float dE = err - errL;
+  errSum += dE;
+  ti_1 = millis()/1000;
+  dt = ti_1 - ti;
 
-void readBT() {
+  int u = floor(kp * err + kd * dE/dt + ki*errSum);
+  errL = err;
+  ti = ti_1;
+  return u;
+  
+}
+
+///////////////////////////////////
+float readBT() {
   cmd2 = Serial1.read();
 
   switch (cmd2) {
     case 's':
       systemState = false;
+      Serial1.println("Stop");
       break;
     case 'g':
-      setA = 0;
-      setB = 0;
+      systemState = true;
+      Serial1.println("Power On");
+      desVelL = 0.5;
+      desVelR = 0.5;
       break;
     case 'u':
-      setA += 10;
-      setB += GAIN * 10;
+      desVelL += 0.5;
+      desVelR += 0.5;
+      Serial1.println(desVel);
       break;
     case 'd':
-      setA -= 10;
-      setB -= GAIN * 10;
+      desVelL -= 0.5;
+      desVelR -= 0.5;
+      Serial1.println(desVel);
       break;
   }
-  if (setA > 255) setA = 255;
-  if (setA < -255) setA = -255;
-  if (setB > 255) setB = 255;
-  if (setB <-255) setB = -255;
+  if (desVelL > 5) desVelL = 5;
+  else if (desVelL < -5) desVelL = -5;
+  if (desVelR > 5) desVelR = 5;
+  else if (desVelR < -5) desVelR = -5;
 }
 
 ///////////////////////////////////
 void Turn(int dir, int setA, int setB) {
   qik.setSpeeds(dir * 255, -GAIN * dir * 255);
-  delay(TURN_TIME);
+  delay(TURN_TIME*abs(dir)/90);
   qik.setSpeeds(setA, setB);
 }
 ///////////////////////////////////
-void getVel() {
+void getVelL() {
   int a1 = leftCount;
-  int a2 = rightCount;
   delay(10);
   int b1 = leftCount;
+  curVelL = ((b1 - a1) / .01) * CPS_TO_MPH;
+}
+///////////////////////////////////
+void getVelR() {
+  int a2 = rightCount;
+  delay(10);
   int b2 = rightCount;
-  velocityL = ((b1 - a1) / .01) * CPS_TO_MPH;
-  velocityR = ((b2 - a2) / .01) * CPS_TO_MPH;
+  curVelR = ((b2 - a2) / .01) * CPS_TO_MPH;
 }
 ///////////////////////////////////
 void leftEnc() {
